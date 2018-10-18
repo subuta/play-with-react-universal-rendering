33f3610ef669af6b340e7f028448a68d928c8e92
subuta <shunta.3110@gmail.com>
2018-10-18 17:10:16 +0900

    Add required props.

--- components/App.js
+++ components/App.js
@@ -8,11 +8,13 @@ export default () => {
       <Helmet>
         <title>play-with-react-universal-rendering</title>
         <meta property='og:title' content='play-with-react-universal-rendering' />
+        <meta property='og:type' content='website' />
         <meta property='og:image' content='http://picsum.photos/1200/630?image=100' />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='630' />
         <meta property='og:url' content='https://play-with-react-universal-rendering.glitch.me/' />
         <meta property='og:description' content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut eros dolor. Pellentesque vestibulum semper turpis, in rutrum est volutpat eget. Ut id tristique magna' />
+        <meta property='fb:app_id' content='dummy' />
       </Helmet>
       */}
       <h1>Hello world!</h1>
