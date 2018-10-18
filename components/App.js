f4f180572f6f64a14751ede418ba3512274fee94
subuta <shunta.3110@gmail.com>
2018-10-18 17:07:14 +0900

    Add width and height.

--- components/App.js
+++ components/App.js
@@ -9,6 +9,8 @@ export default () => {
         <title>play-with-react-universal-rendering</title>
         <meta property='og:title' content='play-with-react-universal-rendering' />
         <meta property='og:image' content='http://picsum.photos/1200/630?image=100' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
         <meta property='og:url' content='https://play-with-react-universal-rendering.glitch.me/' />
         <meta property='og:description' content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut eros dolor. Pellentesque vestibulum semper turpis, in rutrum est volutpat eget. Ut id tristique magna' />
       </Helmet>
